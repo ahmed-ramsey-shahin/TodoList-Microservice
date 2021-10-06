1e71bb402e1f50583fadc63d0a815b4ee9733071
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2021-10-06 21:28:29 +0200

    Changing the main class to be Application

--- config-server/src/main/java/com/ramsey/todolist/configserver/Application.java
+++ config-server/src/main/java/com/ramsey/todolist/configserver/Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		
